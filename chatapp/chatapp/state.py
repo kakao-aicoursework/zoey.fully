c73a2f3aadbb27ca864f5c59e09dfebe773f4b2c
zoey.fully <zoey.fully@kakaocorp.com>
2023-09-19 18:02:21 +0900

    fix: gpt summary 동작하도록 수정

--- chatapp/chatapp/state.py
+++ chatapp/chatapp/state.py
@@ -53,7 +53,7 @@ def get_gpt_keyword(summary_keys, user_sent):
     gpt_select_keyword = completion.choices[0].message.content
     return gpt_select_keyword
 
-def get_gpt_summary(summary_keys):
+def get_gpt_summary(keyword_doc):
     """gpt를 사용한 문장 요약"""
 
     system_msg = {
@@ -94,6 +94,7 @@ def get_gpt_summary(summary_keys):
 file_path = os.path.join(project_path, 'datas/project_data_카카오싱크.txt')  # 실제 파일 경로로 교체
 summary_dict = read_hash_lines(file_path)
 summary_keys = list(summary_dict.keys())
+print("summary_keys:",summary_keys)
 
 class State(rx.State):
     # The current question being asked.
@@ -104,23 +105,23 @@ class State(rx.State):
 
  
 
-    # def get_summary(self, user_sent):
-    #     gpt_select_keyword = get_gpt_keyword(self.summary_keys, user_sent)
-    #     keyword_doc = summary_dict[gpt_select_keyword]
-    #     summary_sent = get_gpt_summary(summary_keys)
-    #     return summary_sent
+    def get_summary(self, user_sent):
+        gpt_select_keyword = get_gpt_keyword(summary_keys, user_sent)
+        keyword_doc = summary_dict[gpt_select_keyword]
+        summary_sent = get_gpt_summary(keyword_doc)
+        return summary_sent
 
     def answer(self):
         # Our chatbot has some brains now!
-        session = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": "user", "content": self.question}
-            ],
-            stop=None,
-            temperature=0.7,
-            stream=True,
-        )
+        # session = openai.ChatCompletion.create(
+        #     model="gpt-3.5-turbo",
+        #     messages=[
+        #         {"role": "user", "content": self.question}
+        #     ],
+        #     stop=None,
+        #     temperature=0.7,
+        #     stream=True,
+        # )
 
         # Add to the answer as the chatbot responds.
         answer = ""
@@ -132,20 +133,19 @@ class State(rx.State):
         # Yield here to clear the frontend input before continuing.
         yield
 
-        # session = ["1","2"]
-        # for item in session:
-        #     answer += self.get_summary(user_sent)
-        #     self.chat_history[-1] = (
-        #                 self.chat_history[-1][0],
-        #                 answer,
-        #             )
-        #     yield
-
-        for item in session:
-            if hasattr(item.choices[0].delta, "content"):
-                answer += item.choices[0].delta.content
-                self.chat_history[-1] = (
+      
+        answer += self.get_summary(self.question)
+        self.chat_history[-1] = (
                     self.chat_history[-1][0],
                     answer,
                 )
-                yield
+        yield
+
+        # for item in session:
+        #     if hasattr(item.choices[0].delta, "content"):
+        #         answer += item.choices[0].delta.content
+        #         self.chat_history[-1] = (
+        #             self.chat_history[-1][0],
+        #             answer,
+        #         )
+        #         yield
